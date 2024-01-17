0649301bc7e78b5c2080b0fcd619d1fd4c9fae85
Aswit0 <dimasaswito@gmail.com>
2024-01-17 19:37:40 +0700

    Type Data

---
+++ typedata.js
@@ -0,0 +1,46 @@
+// String
+let stringVariable = "Hello, World!";
+
+// Number
+let numberVariable = 42;
+
+// BigInt
+let bigIntVariable = 1234567890123456789012345678901234567890n;
+
+// Null
+let nullVariable = null;      //NaN;    //infinity;
+
+// Undefined
+let undefinedVariable;
+
+// Boolean
+let booleanVariable = true;
+
+// Object
+let objectVariable = {
+  key1: "value1",
+  key2: "value2",
+  key3: 3
+};
+
+// Array
+let arrayVariable = [1, 2, 3, "four", true];
+
+// Menampilkan hasil
+console.log("String Variable:", stringVariable);
+console.log("Number Variable:", numberVariable);
+console.log("BigInt Variable:", bigIntVariable);
+console.log("Null Variable:", nullVariable);
+console.log("Undefined Variable:", undefinedVariable);
+console.log("Boolean Variable:", booleanVariable);
+console.log("Object Variable:", objectVariable);
+console.log("Array Variable:", arrayVariable);
+
+
+// Tipe Data Dinamis
+let dynamicVariable = "Ini adalah tipe data dinamis";
+console.log("Dynamic Variable:", dynamicVariable);
+
+dynamicVariable = 42; // Tipe data bisa berubah
+console.log("Dynamic Variable (Updated):", dynamicVariable);
+
